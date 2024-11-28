b2d237e0e4197c2a5b3560d2f919125c7d6d7e32
Pikachoo1111 <armaan20241@outlook.com>
2024-11-28 02:36:08 +0000

    Working firebase account creation

---
+++ firebase.js
@@ -0,0 +1,27 @@
+const firebaseApp = firebase.initializeApp(firebaseConfig = {
+    apiKey: "AIzaSyBl8fFe50U63T8ku-k98RvuZ8lmqSMrjos",
+    authDomain: "impactx-project.firebaseapp.com",
+    projectId: "impactx-project",
+    storageBucket: "impactx-project.firebasestorage.app",
+    messagingSenderId: "980840939930",
+    appId: "1:980840939930:web:2b0e9a8528afac42c5fa23",
+    measurementId: "G-0GMZZKJ8BS"
+  })
+
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+
+const register = () => {
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+    auth.createUserWithEmailAndPassword(email, password)
+    .then((res) => {
+        console.log(res.user)
+    })
+    .catch((error) => {
+        alert(error.message)
+        console.log(error.code)
+        console.log(error.message)
+    })
+}
